2aa4eb0faa17be7c69f8c29ab6313964afc290eb
Ivana2701 <ivanabaj@abv.bg>
2020-09-30 14:06:00 +0000

    .

--- src/exercise1/FdibaOutput.java
+++ src/exercise1/FdibaOutput.java
@@ -6,7 +6,7 @@ public class FdibaOutput {
         System.out.println("F     D    D  II B    B    A A");
         System.out.println("FFFF  D     D II BBBBB    AAAAA");
         System.out.println("F     D    D  II B    B  A     A");
-        System.out.println("F     DDDDD   II BBBBBB A       A");
+        System.out.println("F     DDDDD   II BBBBB  A       A");
         
     }
 
--- src/exercise1/HelloWorld.java
+++ src/exercise1/HelloWorld.java
@@ -3,7 +3,7 @@ package exercise1;
 public class HelloWorld {
 
     public static void main(String[] args) {
-        System.out.print("HelloWorld");
+        System.out.print("HelloWorld!");
     }
 
 }
